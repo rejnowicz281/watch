a5670f33b4a9e44e1375555d6ff7a66be40541d3
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-10-18 19:11:49 +0200

    Allow only signed in users to access the page

---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+    callbacks: {
+        authorized: async ({ req: { cookies } }) => {
+            const secureCookie = process.env.NEXTAUTH_URL?.startsWith("https://") ?? !!process.env.VERCEL;
+            const cookieName = secureCookie ? "__Secure-next-auth.session-token" : "next-auth.session-token";
+            const session = await (
+                await fetch("http://localhost:3000/api/auth/session", {
+                    method: "GET",
+                    headers: { Cookie: `${cookieName}=${cookies.get(cookieName)?.value}` },
+                })
+            ).json();
+            return !!session.user;
+        },
+    },
+});
+
+export const config = { matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|login).*)"] };
