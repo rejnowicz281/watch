d9c8bf14349c5ad4f1caa010e09641b940992f7e
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-11-02 21:15:26 +0100

    Add zero before minutes if it's lower than 10

--- utils/formatSeconds.js
+++ utils/formatSeconds.js
@@ -2,5 +2,5 @@ export default function formatSeconds(seconds) {
     const minutes = Math.floor(seconds / 60);
     const secondsLeft = seconds % 60;
 
-    return `${minutes}:${secondsLeft < 10 ? "0" : ""}${secondsLeft}`;
+    return `${minutes < 10 ? "0" : ""}${minutes}:${secondsLeft < 10 ? "0" : ""}${secondsLeft}`;
 }
