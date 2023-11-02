d8dc768529e73b025413b0fafce9fb6ca15e3428
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-11-02 21:06:55 +0100

    Fix typo

--- app/timers/[id]/components/TimerSettings/index.js
+++ app/timers/[id]/components/TimerSettings/index.js
@@ -7,7 +7,7 @@ import css from "./index.module.css";
 export default function TimerSettings({ name, id, length }) {
     return (
         <>
-            <p class={css.heading}>Settings</p>
+            <p className={css.heading}>Settings</p>
             <EditName name={name} id={id} />
             <EditLength id={id} length={length} />
         </>
