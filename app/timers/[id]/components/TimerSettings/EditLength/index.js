0dce73cbc1a3a893ae2086f2674a43f2fcfeb6d7
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-11-02 20:48:29 +0100

    Have EditLength use server actions

--- app/timers/[id]/components/TimerSettings/EditLength/index.js
+++ app/timers/[id]/components/TimerSettings/EditLength/index.js
@@ -1,20 +1,20 @@
 "use client";
 
 import { updateTimerLength } from "@/actions/timers";
-import AsyncButton from "@/components/AsyncButton";
+import SubmitButton from "@/components/SubmitButton";
 import { useState } from "react";
 
 export default function EditLength({ id, length }) {
     const [lengthInput, setLengthInput] = useState(length);
 
-    async function handleSubmit() {
-        await updateTimerLength(id, lengthInput);
+    function handleAction() {
+        if (lengthInput != "" && lengthInput > 0) updateTimerLength(id, lengthInput);
     }
 
     return (
-        <form onSubmit={(e) => e.preventDefault()}>
+        <form action={handleAction}>
             <input type="number" value={lengthInput} onChange={(e) => setLengthInput(e.target.value)} />
-            <AsyncButton type="submit" mainAction={handleSubmit} content="Update Length" loadingContent="Updating..." />
+            <SubmitButton submitContent="Update Length" submittingContent="Updating..." />
         </form>
     );
 }
